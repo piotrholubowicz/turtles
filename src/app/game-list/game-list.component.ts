54c6b15bbce65740dfbc659d39716700a20cd104
piotrholubowicz <piotrekhol@gmail.com>
2020-03-23 10:57:51 -0700

    Added a games list.

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -1,4 +1,11 @@
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { GameService }  from '../game.service';
+import { Game } from '../game';
 
 @Component({
   selector: 'app-game-list',
@@ -6,10 +13,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-list.component.css']
 })
 export class GameListComponent implements OnInit {
+  games$: Observable<Game[]>;
+  selectedId: string;
 
-  constructor() { }
+  constructor(
+    private service: GameService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.games$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        this.selectedId = params.get('id');
+        return this.service.getGames();
+      })
+    );
   }
-
 }
