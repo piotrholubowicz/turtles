2c066866b123c346e5362618193e59f6dee71a03
piotrholubowicz <piotrekhol@gmail.com>
2020-03-31 21:59:19 -0700

    Uses etags.

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-import { Observable, of, timer, Subject } from "rxjs";
+import { Observable, of, timer, Subject, empty } from "rxjs";
 import { catchError, map, tap, switchMap, distinctUntilChanged, share } from "rxjs/operators";
 
 import { Game, Color } from "./game";
@@ -17,10 +17,9 @@ export class GameService {
     share(),  // create a new Subject, which will act as a proxy
   );
   private perGame$ = new Map<number, Observable<Game>>();
+  private cache: { [url: string]: string; } = {};  // url => etag
 
-  httpOptions = {
-    headers: new HttpHeaders({ "Content-Type": "application/json" })
-  };
+  private headers = new HttpHeaders({ "Content-Type": "application/json" })
 
   constructor(private http: HttpClient) {}
 
@@ -30,10 +29,12 @@ export class GameService {
   }
 
   fetchGames(): Observable<Game[]> {
-    return this.http.get<Game[]>(this.gamesUrl).pipe(
-      // tap(_ => console.log("fetched games")),
-      distinctUntilChanged(),
-      catchError(this.handleError<Game[]>("getGames", []))
+    const url = this.gamesUrl;
+    const headers = this.cache[url] ? this.headers.set('If-None-Match', this.cache[url]) : this.headers;
+    return this.http.get<Game[]>(url, { observe: 'response', headers: headers }).pipe(
+      tap(resp => this.cache[url] = resp.headers.get('Etag')),
+      map(resp => resp.body),
+      catchError(this.handleError<Game[]>("getGames"))
     );
   }
 
@@ -51,9 +52,10 @@ export class GameService {
 
   fetchGame(id: number): Observable<Game> {
     const url = `${this.gamesUrl}/${id}`;
-    return this.http.get<Game>(url).pipe(
-      // tap(_ => console.log(`fetched game id=${id}`)),
-      distinctUntilChanged(),
+    const headers = this.cache[url] ? this.headers.set('If-None-Match', this.cache[url]) : this.headers;
+    return this.http.get<Game>(url, { observe: 'response', headers: headers }).pipe(
+      tap(resp => this.cache[url] = resp.headers.get('Etag')),
+      map(resp => resp.body),
       catchError(this.handleError<Game>(`getGame id=${id}`))
     );
   }
@@ -62,7 +64,7 @@ export class GameService {
 
   /** POST: add a new game to the server */
   addGame(game: Game): Observable<Game> {
-    return this.http.post<Game>(this.gamesUrl, game, this.httpOptions).pipe(
+    return this.http.post<Game>(this.gamesUrl, game, { headers: this.headers }).pipe(
       tap((newGame: Game) => console.log(`added game w/ id=${newGame.id}`)),
       tap((game: Game) => console.log(game)),
       catchError(this.handleError<Game>("addGame"))
@@ -74,7 +76,7 @@ export class GameService {
     const id = typeof game === "number" ? game : game.id;
     const url = `${this.gamesUrl}/${id}`;
 
-    return this.http.delete<Game>(url, this.httpOptions).pipe(
+    return this.http.delete<Game>(url, { headers: this.headers }).pipe(
       tap(_ => console.log(`deleted game id=${id}`)),
       catchError(this.handleError<Game>("deleteHero"))
     );
@@ -84,7 +86,7 @@ export class GameService {
   updateGame(game: Game): Observable<any> {
     const url = `${this.gamesUrl}/${game.id}`;
     console.log(game);
-    return this.http.put(url, game, this.httpOptions).pipe(
+    return this.http.put(url, game, { headers: this.headers }).pipe(
       tap(_ => console.log(`updated game id=${game.id}`)),
       catchError(this.handleError<any>("updateGame"))
     );
@@ -96,16 +98,15 @@ export class GameService {
    * @param operation - name of the operation that failed
    * @param result - optional value to return as the observable result
    */
-  private handleError<T>(operation = "operation", result?: T) {
+  private handleError<T>(operation = "operation", fallback: Observable<T> = empty()) {
     return (error: any): Observable<T> => {
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      console.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
+      if (error.status == 304) {
+        // This is working as intended
+      } else {
+        console.error(error); // log to console instead
+        console.log(`${operation} failed: ${error.message}`);
+      }
+      return fallback;
     };
   }
 }
