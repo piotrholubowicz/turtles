5f6dd7e0d683745471aaa8411004f835ddf8e870
Piotr Hołubowicz <piotrekhol@gmail.com>
2020-04-18 17:47:05 -0700

    failed again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   name = 'Turtles';
+  languageList = [
+    { code: 'en', label: 'English' },
+    { code: 'pl', label: 'Polski' },
+  ];
 
-  constructor() {}
+  constructor(@Inject(LOCALE_ID) protected localeId: string) {}
 }
